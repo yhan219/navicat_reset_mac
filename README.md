fb4026a648162b49e8671e00a95677a80f4ee636
yhan219 <yhan219@sina.com>
2022-10-05 22:32:32 +0800

    添加52破解提供的脚本

--- README.md
+++ README.md
@@ -36,7 +36,8 @@
 - 按照原理说明查看对应的数据是否删除成功
 
 ## 尝试其他人的脚本
-可尝试[52破解](https://www.52pojie.cn/forum.php?mod=viewthread&tid=1669993)提供的脚本试试看，代码复制了一份到`reset_navicat_52pojie.sh`,侵删。
+可尝试`reset_navicat_52pojie.sh`脚本，由[52破解](https://www.52pojie.cn/forum.php?mod=viewthread&tid=1669993)提供,原理是一样的，感谢
+@Dr-Octopus-dev 提供 #16。**侵删**。
 
 ## 许可证
 
---
+++ reset_navicat_52pojie.sh
@@ -0,0 +1,50 @@
+
+# 代码来源 https://www.52pojie.cn/forum.php?mod=viewthread&tid=1669993
+
+#!/bin/bash
+
+set -e
+
+file=$(defaults read /Applications/Navicat\ Premium.app/Contents/Info.plist)
+
+regex="CFBundleShortVersionString = \"([^\.]+)"
+[[ $file =~ $regex ]]
+
+version=${BASH_REMATCH[1]}
+
+echo "Detected Navicat Premium version $version"
+
+case $version in
+    "16")
+        file=~/Library/Preferences/com.navicat.NavicatPremium.plist
+        ;;
+    "15")
+        file=~/Library/Preferences/com.prect.NavicatPremium15.plist
+        ;;
+    *)
+        echo "Version '$version' not handled"
+        exit 1
+       ;;
+esac
+
+echo -n "Reseting trial time..."
+
+regex="([0-9A-Z]{32}) = "
+[[ $(defaults read $file) =~ $regex ]]
+
+hash=${BASH_REMATCH[1]}
+
+if [ ! -z $hash ]; then
+    defaults delete $file $hash
+fi
+
+regex="\.([0-9A-Z]{32})"
+[[ $(ls -a ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/ | grep '^\.') =~ $regex ]]
+
+hash2=${BASH_REMATCH[1]}
+
+if [ ! -z $hash2 ]; then
+    rm ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/.$hash2
+fi
+
+echo " Done"
