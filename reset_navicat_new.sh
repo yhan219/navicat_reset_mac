534d449c9e63c8a4f616e1ef35edd5ab14491a4d
yhan219 <yhan219@163.com>
2023-10-23 15:11:21 +0800

    Create reset_navicat_new.sh
    高版本重置方法

---
+++ reset_navicat_new.sh
@@ -0,0 +1,11 @@
+# copy from https://github.com/pretend-m/navicat_for_mac_reset
+rm -rf ~/Library/Preferences/com.navicat.NavicatPremium.plist
+
+regex="\.([0-9A-Z]{32})"
+[[ $(ls -a ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/ | grep '^\.') =~ $regex ]]
+
+hash=${BASH_REMATCH[1]}
+
+if [ ! -z $hash ]; then
+    rm ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/.$hash
+fi
