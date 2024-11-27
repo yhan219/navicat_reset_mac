e176dfac19cf5e0e3038222a92223c54ca20002f
srk24 <25836646+srk24@users.noreply.github.com>
2024-11-27 10:08:15 +0800

    fix error

--- reset_navicat.sh
+++ reset_navicat.sh
@@ -4,4 +4,4 @@
 /usr/libexec/PlistBuddy -c "Delete :91F6C435D172C8163E0689D3DAD3F3E9" ~/Library/Preferences/com.navicat.NavicatPremium.plist
 /usr/libexec/PlistBuddy -c "Delete :B966DBD409B87EF577C9BBF3363E9614" ~/Library/Preferences/com.navicat.NavicatPremium.plist
 
-rm -rf ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/.*
+rm -rf ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/*
